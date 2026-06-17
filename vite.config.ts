965475e659e31944f80d22d842bfd1b4ef5e8af4
fajmuls <mrachmanfm@gmail.com>
2026-06-17 15:32:48 +0700

    fix(config): update entry path and resolve __dirname
    Use absolute path for index.tsx and resolve __dirname for compatibility with ES modules in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     
